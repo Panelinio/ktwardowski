f3df45d3cb33a75df8aca9ea007d22d722b317e7
Kacper <kacper.tw@wp.pl>
2024-09-05 15:59:15 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://Panelinio.github.io',
-    base: 'ktwardowski'
-});
+export default defineConfig({});
