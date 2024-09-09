a5451820b8210dfc5db9f3281b32c064d3e7a9b8
Kacper <kacper.tw@wp.pl>
2024-09-09 15:23:47 +0200

    Fixing config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://panelinio.github.io',
-    base: 'ktwardowski',
-  })
+    site: 'https://Panelinio.github.io',
+    base: 'ktwardowski'
+});
