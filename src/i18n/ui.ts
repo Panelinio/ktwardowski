74a393a041b505b3b99c42a24ccab4e434b1426b
Kacper <kacper.tw@wp.pl>
2024-09-16 11:08:16 +0200

    Adding english language

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+    en: 'English',
+    pl: 'Polski',
+  };
+  
+  export const defaultLang = 'pl';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.contact': 'Contakt',
+      'footer.text': 'Find me in the Web',
+    },
+    pl: {
+      'nav.home': 'Strona główna',
+      'nav.about': 'O mnie',
+      'nav.contact': 'Kontakt',
+      'footer.text': 'Znajdź mnie w sieci',
+    },
+  } as const;
+
+  export const showDefaultLang = false;
+
+  
