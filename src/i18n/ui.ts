a99fcbe0666acf9935a9d795abedea6ee449d99b
Kacper <kacper.tw@wp.pl>
2024-09-16 14:36:01 +0200

    Fixed language switcher - TODO: fix css and translate pages

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,17 +1,19 @@
 export const languages = {
-    en: 'English',
-    pl: 'Polski',
-  };
-  
-  export const defaultLang = 'en';
-  
-  export const ui = {
-    en: {
-      'nav.home': 'Home',
-      'nav.about': 'About',
-    },
-    pl: {
-      'nav.home': 'Strona główna',
-      'nav.about': 'O mnie',
-    },
-  } as const;
+  en: 'English',
+  pl: 'Polski',
+};
+
+export const defaultLang = 'pl';
+
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+  },
+  pl: {
+    'nav.home': 'Strona główna',
+    'nav.about': 'O mnie',
+  },
+} as const;
