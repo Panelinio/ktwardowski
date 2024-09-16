3e516325b0499861302b2cb2fd547fe9f860d2bf
Kacper <kacper.tw@wp.pl>
2024-09-16 12:08:05 +0200

    Fixing language switcher

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+    en: 'English',
+    pl: 'Polski',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+    },
+    pl: {
+      'nav.home': 'Strona główna',
+      'nav.about': 'O mnie',
+    },
+  } as const;
